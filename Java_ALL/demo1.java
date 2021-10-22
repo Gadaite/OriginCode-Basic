f59b605b807161ca169e7f3eaf4aacf9eacfffcf
Gadaite <rangeyang22@gmail.com>
2021-10-22 17:40:22 +0800

    mycode

--- Java_ALL/demo1.java
+++ Java_ALL/demo1.java
@@ -1,10 +1,6 @@
 import java.util.Scanner;
-import org.apache.spark.SparkContext;
-import org.apache.spark.SparkContext._;
-import org.apache.spark.SparkConf;
 public class demo1{
     public static void main(String[] args){
-        val conf = new SparkConf();
-        val sc = new SparkContext(conf);
+        System.out.println("hello ");
     }
 }
