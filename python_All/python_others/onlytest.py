403ccd0c47b8bb0a407c60960acea13adf36d377
Gadaite <rangeyang22@gmail.com>
2021-10-26 14:57:19 +0800

    first commit

--- python_All/python_others/onlytest.py
+++ python_All/python_others/onlytest.py
@@ -1,15 +1,22 @@
+
 #%%
-dict = {"a":{"b":1,"c":2,"d":3}}
-print(type(dict["a"]))
-dict["a"]
+import time
+import random
+start=time.time()
+y=[random.randint(1,10000000000) for i in range(0,100)]
+y
 #%%
-print(type(dict["a"]["b"]))
-dict["a"]["b"]
-#%%
-type(dict)
-#%%
-import json
-r = [{'b':1,'c':2,'d':3}]
-w = r[0]
-ss = json.loads(w)
-ss['b']
+count =0 
+for i in range(0,99):
+    for j in range(i+1,100):
+        if y[i] > y[j]:
+            y[i], y[j]=y[j], y[i]
+        count = count+1
+        if(time.time()-start >= 20):
+            break
+
+print(y)
+end=time.time()
+print(end-start)
+# %%
+print("hello !!")
