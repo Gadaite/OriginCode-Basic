d0807b29893cfbfc67d93078d8209e557d562b54
Gadaite <rangeyang22@gmail.com>
2021-11-15 10:24:06 +0800

    Gadaite

---
+++ spark_object/python_All/python_ML/Numpy/axis_loadtxt.py
@@ -0,0 +1,29 @@
+# axis维度的概念，按照块往下数
+
+
+# numpy也可以读取数据，从csv里面读取数据，使用loadtxt
+
+# fname:要读取的文件、文件名或生成器
+# dtype:可选数据类型默认为float 
+# delimiter:分割字符串默认是空格，可设置逗号等
+# skiprows:跳过前多少行
+# usecols:读取指定列
+# unpack:True写入不同数据变量，FALSE写入一个数组变量
+
+#%%
+import numpy as np
+import random
+from numpy.core.fromnumeric import shape
+x= np.array([random.randint(10,20) for i in range(100)]).reshape(10,10)
+shape(x)
+
+for i in range(0,10):
+    for j in range(0,10):
+        print(x[i][j],end=" ")
+    print()
+
+path = "/root/Github_files/spark_object/python_All/Dataset/线性不可分类别.csv"
+csvdata = np.loadtxt(fname =path,dtype=float,skiprows=1,delimiter=",")
+csvdata
+
+
--- spark_object/python_All/python_ML/Numpy/basic1.py
+++ spark_object/python_All/python_ML/Numpy/basic1.py
@@ -1,4 +1,14 @@
 #%%
+# 基本数据以及画图
 import numpy as np
-t1 = np.arange(12)
-t1
+from numpy.core.fromnumeric import shape
+x= np.arange(0,100,0.1)
+shape(x)
+y = np.sin(x)
+import matplotlib.pyplot as plt
+plt.figure(figsize=(20,8),dpi=80)
+plt.plot(x,y,LineStyle ="-.",color ="red",label ="sin(x)")
+# plt.figure(figsize=(20,8),dpi=80)
+plt.legend(loc = "upper left")
+plt.grid()
+plt.show()
