f4e481ebf13e3086d59200bd4b195a4b3ea331f7
Gadaite <rangeyang22@gmail.com>
2021-11-25 17:27:26 +0800

    Gadaite

--- spark_object/python_All/python_ML/Z_Others/opencv_image_002.py
+++ spark_object/python_All/python_ML/Z_Others/opencv_image_002.py
@@ -3,6 +3,7 @@ import cv2
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.core.fromnumeric import shape
 #%%
 #创建图像锐化的核
 kernel = np.array([[0, -1, 0],\
@@ -33,8 +34,19 @@ plt.imshow(image_color_YUV)
 #step2:对图像应用直方图均衡
 """简单来说，就是它会转换图像，使
 像素强度的分布范围更广"""
-image_color_YUV[:][:][0] = cv2.equalizeHist(image_color_YUV[:][:][0])
+image_color_YUV[:][:][0] = cv2.equalizeHist(image_color_YUV[:][:][6])
 plt.imshow(image_color_YUV)
+#%%
+image_color_YUV
+#%%
+print(type(image_color_YUV))
+print(np.shape(image_color_YUV))
+#%%
+import random
+x= [random.randint(10,50) for i in range(12)]#list
+test = np.array(x).reshape(2,6)
+print(type(test))
+plt.plot(test)
 #%%
 #step3:转换成RGB 格式
 image_color_RGB = cv2.cvtColor(image_color_YUV,cv2.COLOR_YUV2RGB)
--- spark_object/src/main/scala/others/liyao.scala
+++ spark_object/src/main/scala/others/liyao.scala
@@ -1,7 +1,20 @@
 package others
-import org.apache.spark.mllib.optimization
+
+import org.apache.spark.sql.SparkSession
+// import org.apache.spark.mllib.classification.svc.{SVCModel, SVCModelParam}
+import org.apache.spark.mllib.feature.StandardScaler
+import org.apache.spark.mllib.linalg.Vectors
+// import org.apache.spark.mllib.optimization.smo.kernel.{LinearFunction, PolyFunction, RBFFunction}
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 object  liyao extends App{
     override def main(args: Array[String]): Unit = {
-        
+        val spark = SparkSession.builder().appName("app").master("local[*]").getOrCreate()
+        val sc =spark.sparkContext
+        val rdd = sc.parallelize(List(1,2,3,4,5,6))
+        print(rdd.collect().foreach(x =>print(x.toString()+" ")))
     }
 }
