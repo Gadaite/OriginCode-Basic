827c18ac1e642c24b4ed51103d05b6ae08d129e1
Gadaite <rangeyang22@gmail.com>
2022-01-01 12:09:23 +0800

    Gadaite

---
+++ spark_object/python_All/Win_localPy/pyspark_SQL/pysaprkPostgresql.py
@@ -0,0 +1,12 @@
+#%%
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("app").master("local[*]").getOrCreate()
+jdbcdf = spark.read.format("jdbc")\
+            .option("driver","org.postgresql.Driver")\
+            .option("url","jdbc:postgresql://139.155.70.177:5432/trajectory")\
+            .option("dbtable","lastappeared")\
+            .option("user","postgres")\
+            .option("password","zzjz123")\
+            .load()
+jdbcdf.show()
+
